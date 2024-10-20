e55a6302e89ee19c6cedcffdaa2aa76995378aa9
AlexSulll <hlebusheck.tv@gmail.com>
2024-10-21 00:00:08 +0300

    puck

--- qqq.asm
+++ qqq.asm
@@ -1,14 +1,9 @@
 .model      small
 .186
 .stack      100h
-.data
-   A db 127
-
 .code
 Start:
     mov     ax,@data
     mov     ds,ax
-    mov     al,[A]
-    
-    aam 
-    end Start
+    mov     ax, 127d
+end Start
--- test.asm
+++ test.asm
@@ -1,95 +1,89 @@
 .model      small
 .186
 .stack      100h
+MIN     =   -128d
+MAX     =   127d
 .data
     fileName    db      'overflow.txt',0
-    zapis       db      'A = 0000, B = 0000, C = 0000', 0ah ;29 simvolov
+    zapis       db      'A = 0000, B = 0000, C = 0000', 0Dh, 0ah ;30 simvolov
     A           db      ?
     B           db      ?
     C           db      ?
-    countA      db      ?
-    countB      db      ?
-    countC      db      ?
-    idfile      dw      ?
-
+    result      dw      ?
 .code
 Start:
     mov     ax,@data
     mov     ds,ax
-    mov     al,[C] ; al = C
-    or      al,[A] ; A = C
-    jnz     Answer ; if A != 0 and C != 0 goto Answer
-    mov     [A],-128
-    mov     [countA],255
-    
+    mov     bx,word ptr [A]
+    mov     ch,[C]
+    mov     al,ch
+    or      al,bh
+    or      al,bl
+    jnz     Answer   
 CreateFile:
     mov     ah,3Ch
     mov     cx,00100000b
     mov     dx,offset fileName
     int     21h
-    mov     [idfile],ax
-    mov     di,[idfile]
-   
-a_loop:
-    mov     [countB],255
-    mov     [B],-128
-    
-b_loop:
-    mov     [countC],255
-    mov     [C],-128
-    
-c_loop:
-    ;go to answer
-    
+    mov     cl,al  
+setvalue:
+    mov     bx,MIN
+    mov     bh,bl
+    mov     ch,bl  
 Answer:
-    ;znamenatel
-    mov     al,B
+    mov     al,bh
     cbw
-    mov     bl,A
-    xchg    ax,bx   ; ax-A,bx-B
-    cbw             ; al to ax
-    imul    bx      ; ax=A*B(ax*bx)
-    sal     ax,1    ; ax = 2*ax
-    mov     cx,ax   ; cx = 2*ax
-    sal     ax,1    ; ax = 4*ax
-    add     cx,ax   ; cx = cx + ax (6*A*B)
-    mov     al,C    ; al = C
-    cbw             ; al to cbw ax
-    add     cx,ax   ; cx = cx + ax
-    jcxz    Write_A
-    jo      Write_A ;TODO!!!
-    ;chislitel
-    mov     ax,bx   ; ax = bx(B)
-    imul    bx      ; ax = ax * bx (B^2)
-    imul    bx      ; ax = ax * bx (B^3)
-    mov     bx,ax   ; bx = B^3
-    sal     ax,5    ; ax = 32*ax
-    sal     bx,2    ; bx = 4*bx (4*ax)
-    sub     ax,bx   ; ax = 28*B^3
-    mov     bl,C
-    xchg    ax,bx   ; bx=28*b^3, al = C
-    cbw             ; ax
-    add     ax,bx
-    sub     ax,19
-    cwd             ; dx:ax = ax
-    idiv    cx      ; ax = answer
-    or      di,00000h
-    jnz     not_exit
-    jmp     Exit
+    mov     dl,bl
+    xchg    ax,dx
+    cbw
+    mov     bp,ax
+    sal     bp,1
+    sal     ax,2
+    add     ax,bp ;ax = 6ab
     
+    
+    
+    ;mov     al,B
+    ;cbw
+    ;mov     bl,A
+    ;xchg    ax,bx   ; ax-A,bx-B
+    ;cbw             ; al to ax
+    ;imul    bx      ; ax=A*B(ax*bx)
+    ;sal     ax,1    ; ax = 2*ax
+    ;mov     cx,ax   ; cx = 2*ax
+    ;sal     ax,1    ; ax = 4*ax
+    ;add     cx,ax   ; cx = cx + ax (6*A*B)
+    ;mov     al,C    ; al = C
+    ;cbw             ; al to cbw ax
+    ;add     cx,ax   ; cx = cx + ax
+    ;jcxz    Write_A
+    ;jo      Write_A ;TODO!!!
+    ;chislitel
+    ;mov     ax,bx   ; ax = bx(B)
+    ;imul    bx      ; ax = ax * bx (B^2)
+    ;imul    bx      ; ax = ax * bx (B^3)
+    ;mov     bx,ax   ; bx = B^3
+    ;sal     ax,5    ; ax = 32*ax
+    ;sal     bx,2    ; bx = 4*bx (4*ax)
+    ;sub     ax,bx   ; ax = 28*B^3
+    ;mov     bl,C
+    ;xchg    ax,bx   ; bx=28*b^3, al = C
+    ;cbw             ; ax
+    ;add     ax,bx
+    ;sub     ax,19
+    ;cwd             ; dx:ax = ax
+    ;idiv    cx      ; ax = answer
+    ;or      di,00000h
+    ;jnz     not_exit
+    ;jmp     Exit  
+   jmp Write_A 
 not_exit:
     jmp next_itter
-
 Write_A:
+    mov     al,bl
+    cmp     al,0
+    jl      otr_A
     mov     [zapis + 4],"+"
-    mov     al,A
-    test    al,080h
-    jns     Chislo_A
-    neg     al
-    mov     [zapis + 4],"-"
-    ;cmp     al,0
-    ;jl      otr_A
-    
 Chislo_A:
     aam
     or      al,30h
@@ -99,18 +93,12 @@ Chislo_A:
     or      al,30h
     mov     [zapis + 6], al
     or      ah,30h
-    mov     [zapis + 5], ah
-       
+    mov     [zapis + 5], ah    
 Write_B:
-    mov     [zapis + 14],"+"
-    mov     al,B
-    test    al,080h
-    jns     Chislo_B
-    neg     al
-    mov     [zapis + 14],"-"
-    ;cmp     al,0
-    ;jl      otr_B
-    
+    mov     al,bh
+    cmp     al,0
+    jl      otr_B
+    mov     [zapis + 14],"+" 
 Chislo_B:
     aam
     or      al,30h
@@ -121,17 +109,11 @@ Chislo_B:
     mov     [zapis + 16], al
     or      ah,30h
     mov     [zapis + 15], ah
-    
 Write_C:
+    mov     al,ch
+    cmp     al,0
+    jl      otr_C
     mov     [zapis + 24],"+"
-    mov     al,C
-    test    al,080h
-    jns     Chislo_C
-    neg     al
-    mov     [zapis + 24],"-"
-    ;cmp     al,0
-    ;jl      otr_C
-
 Chislo_C:
     aam
     or      al,30h
@@ -142,53 +124,49 @@ Chislo_C:
     mov     [zapis + 26], al
     or      ah,30h
     mov     [zapis + 25], ah
-      
 Write:
+    mov     bp,cx
+    mov     di,bx
     mov     ah,40h
-    mov     bx,idfile
+    mov     bl,cl
+    xor     bh,bh
     mov     dx,offset zapis
-    mov     cx,29
+    mov     cx,30
     int     21h
+    mov     cx,bp
+    mov     bx,di
     jmp     next_itter
-
-;otr_A:
-;    mov     [zapis + 4],"-"
-;    jmp     Chislo_A
-;    
-;otr_B:
-;    mov     [zapis + 14],"-"
-;    jmp     Chislo_B
-;    
-;otr_C:
-;    mov     [zapis + 24],"-"
-;    jmp     Chislo_C
-    
+otr_A:
+    neg     al
+    mov     [zapis + 4],"-"
+    jmp     Chislo_A
+otr_B:
+    neg     al
+    mov     [zapis + 14],"-"
+    jmp     Chislo_B
+otr_C:
+    neg     al
+    mov     [zapis + 24],"-"
+    jmp     Chislo_C
 next_itter:
-    inc     [C]
-    dec     [countC]
-    cmp     [countC],-1
-    jz      cCount_0
-    jmp     c_loop
-        
-cCount_0:  
-    inc     [B]
-    dec     [countB]
-    cmp     [countB],-1
-    jz      bCount_0
-    jmp     b_loop
-    
-bCount_0:
-    inc     [A]
-    dec     [countA]
-    cmp     [countA],-1
-    jz      aCount_0
-    jmp     a_loop
-    
-aCount_0:
-    
-    
+    cmp     ch,MAX
+    jl      c_iter
+    cmp     bh,MAX
+    jl      b_iter
+    cmp     bl,MAX
+    jnl     Exit
+a_iter:
+    inc     bl
+    mov     bh,MIN-1
+b_iter:
+    inc     bh
+    mov     ch,MIN-1
+c_iter:
+    inc     ch
+not_max:
+    jmp     Answer
 Exit:
     mov     ah,04ch
     mov     al,0
     int     21h
-    end     Start
+    end     Start   
